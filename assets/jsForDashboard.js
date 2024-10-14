9facf83cf761a8a5cfb8f898edd50ea57e15ca78
DeRick546 <cuij0003@hz.com>
2024-10-14 13:27:18 +0200

    commit

--- assets/jsForDashboard.js
+++ assets/jsForDashboard.js
@@ -1,85 +1,44 @@
-// code for dashboard
-
-const progressBar = document.getElementById('progress-bar');
-const percentageLabel = document.getElementById('percentage-label');
-
-let currentProgress = 0;
-const maxProgress = 60;
-// add 1 to bar = link 
-incrementButton1.addEventListener('click', () => {
-  if (currentProgress < maxProgress) {
-    currentProgress++;
-    updateProgressBar();
+//code voor input
+function validateInput(int) {
+  const intInput = document.getElementById(`integerInput${int}`);
+  const input = intInput.value;
+  if (input >= 5.5) {
+    document.getElementById(`message${int}`).innerHTML = `Het Cijfer is: ${input}`;
+    let totalEc = 0;
+    if (intInput) {
+      let currentElement = intInput.parentElement;
+      while (currentElement) {
+        if (currentElement.querySelector('.ECstring')) {
+          const aantalECElement = currentElement.querySelector('.ECstring');
+          totalEc += parseFloat(aantalECElement.innerHTML.slice(0, -3)) || 0;
+          break;
+        }
+        currentElement = currentElement.parentElement;
+      }
+    }
+    addIntToPRB(totalEc);
+  } else {
+    document.getElementById(`message${int}`).innerHTML = `Het cijfer is onder 5,5 dus moet je deze les herkansen.`;
   }
-});
-
-function updateProgressBar() {
-  const progressPercentage = (currentProgress / maxProgress) * 100;
-  progressBar.style.width = progressPercentage + '%';
-  percentageLabel.textContent = currentProgress + "EC's";
+  deleteElement(`integerInput${int}`)
+  deleteElement(`labelCijfer${int}`)
+  deleteElement(`buttonClick${int}`)
 }
 function deleteElement(DeleteId) {
   document.getElementById(DeleteId).remove()
 }
-
-//main base of ops!
-function validateInput(int) {
-  const arrayinput = [
-    document.getElementById('integerInput0').value,document.getElementById('integerInput1').value,
-    document.getElementById('integerInput2').value,document.getElementById('integerInput3').value,
-    document.getElementById('integerInput4').value,document.getElementById('integerInput5').value,
-    document.getElementById('integerInput6').value,document.getElementById('integerInput7').value,
-    document.getElementById('integerInput8').value,document.getElementById('integerInput9').value,
-    document.getElementById('integerInput10').value,document.getElementById('integerInput11').value,
-    document.getElementById('integerInput12').value,document.getElementById('integerInput13').value,
-    document.getElementById('integerInput14').value,document.getElementById('integerInput15').value,
-    document.getElementById('integerInput16').value]
-
-    const input = arrayinput[int]
-  const arraymessage = [
-    ,document.getElementById('message0'),document.getElementById('message1'),document.getElementById('message2'),
-    ,document.getElementById('message3'),document.getElementById('message4'),document.getElementById('message5')
-    ,document.getElementById('message6'),document.getElementById('message7'),document.getElementById('message8')
-    ,document.getElementById('message9'),document.getElementById('message10'),document.getElementById('message11')
-    ,document.getElementById('message12'),document.getElementById('message13'),document.getElementById('message14')
-    ,document.getElementById('message15'),document.getElementById('message16'),]
-
-  if (int == 0){
-   
-    //addIntToPRB()
-    if (input >= 5.5) {
-      document.getElementById('message0').innerHTML = `Het Cijfer is: ${input}`;
-      //addIntToPGB()
-    } else {
-      document.getElementById('message0').innerHTML = `Het cijfer is onder 5,5 dus moet je deze les herkansen.`;
-    }
-    deleteElement('integerInput0')
-    deleteElement('labelCijfer0')
-    deleteElement('buttonClick0')
-  }
-  if (int == 1){
-    console.log("werkt if1t")
-    //addIntToPRB()
-    if (input >= 5.5) {
-      document.getElementById('message1').innerHTML = `Het Cijfer is: ${input}`;
-      //addIntToPGB()
-    } else {
-      document.getElementById('message1').innerHTML = `Het cijfer is onder 5,5 dus moet je deze les herkansen.`;
-    }
-    deleteElement('integerInput1')
-    deleteElement('labelCijfer1')
-    deleteElement('buttonClick1')
-  }
-}
+//progressbar
+let currentProgress = 0;
+const maxProgress = 60;
 
 function addIntToPRB(intEC) {
+  const progressBar = document.getElementById('progress-bar-dashboard');
+  const percentageLabel = document.getElementById('percentage-label');
   if (currentProgress < maxProgress) {
     currentProgress = currentProgress + intEC;
-    updateProgressBar();
+    const progressPercentage = (currentProgress / maxProgress) * 100;
+    progressBar.style.width = `${progressPercentage}%`;
+    percentageLabel.textContent = `${currentProgress} EC's`;
+    document.getElementById(`percentage-label`).innerHTML = `${currentProgress} EC's`;
   }
-}
-function updateProgressBar() {
-  const progressPercentage = (currentProgress / maxProgress) * 100;
-  progressBar.style.width = progressPercentage + '%';
-  percentageLabel.textContent = currentProgress + "EC's";
 }
